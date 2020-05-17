ceb039bd05ecdb629fe46f011e602bfdb6c2fb61
Stuart Smith <Stuart.Smith@hansencx.com>
2020-05-17 16:49:39 -0700

    Updating App.config files getting application to run, Tested copying back up files

---
+++ WfFileWatcherAndTransferCmd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using WfFileWatcherAndTransferLib;
+
+
+namespace WfFileWatcherAndTransferCmd
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                ZipWatcherPreferences zInit = new ZipWatcherPreferences();
+                zInit.PopulateFromConfigurationFile();
+
+
+
+                string outputtext = "---------------------------------------------------------------------\n" +
+                                    "Starting application for watching a specific folder \n" +
+                                    "and then zipping the files added to said folder\n\n\n" +
+                                    "File Watcher Parameters:\n" +
+                                    $"Folder to Watch:           {zInit.PathToWatch}\n" +
+                                    $"Zipping Output folder:     {zInit.OutPutFolderFor7zip}\n" +
+                                    $"Path To 7zip:              {zInit.PathTo7Zip}\n" +
+                                    "---------------------------------------------------------------------";
+
+                WfFileWatcherEventLog.Instance.LogMessage(outputtext);
+
+                DirectoryZipWatcher dZipWatch = new DirectoryZipWatcher();
+                dZipWatch.StartWatch(zInit);
+
+                Console.WriteLine("");
+
+                Console.WriteLine("Press Any Key to Continue");
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
